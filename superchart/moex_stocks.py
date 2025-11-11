3a9010f8422067f0b5e7db88c9088a0a8d6b147d
Emil Enikeev <enikeev@pm.me>
2025-11-11 17:53:09 +0300

    feat: cache

--- superchart/moex_stocks.py
+++ superchart/moex_stocks.py
@@ -15,6 +15,7 @@ from sqlalchemy import (
 )
 
 load_dotenv()
+CACHE_TTL_SECONDS = 300
 
 
 class APIMOEXError(Exception):
@@ -44,10 +45,13 @@ async def load_data_neon(query):
         await engine.dispose()
 
 
+@st.cache_data(show_spinner=False, ttl=CACHE_TTL_SECONDS)
 def load_data_neon_sync(table):
     df = asyncio.run(load_data_neon(f"select * from {table}"))
     return df
 
+
+@st.cache_data(show_spinner=False, ttl=CACHE_TTL_SECONDS)
 def load_data_neon_base_dict(ticker):
     df = asyncio.run(load_data_neon(f"select * from base_dict where ticker = '{ticker}'"))
     return df
